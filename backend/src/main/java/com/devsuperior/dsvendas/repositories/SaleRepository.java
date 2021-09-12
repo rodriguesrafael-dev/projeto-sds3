fc693b0d48e97c992b4a37e6714f8e3f9d3998de
rodriguesrafael-dev <rodriguesrafael@RODRAF83-SQ52298.localdomain>
2021-09-12 20:54:20 -0300

    commit 09.12.2021

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -1,7 +1,11 @@
 package com.devsuperior.dsvendas.repositories;
 
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  *
@@ -9,4 +13,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("SELECT new com.devsuperior.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+            + "FROM Sale AS obj GROUP BY obj.seller")
+    List<SaleSumDTO> amountGroupedBySeller();
+    
+    @Query("SELECT new com.devsuperior.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+            + "FROM Sale AS obj GROUP BY obj.seller")
+    List<SaleSuccessDTO> successGroupedBySeller();
 }
