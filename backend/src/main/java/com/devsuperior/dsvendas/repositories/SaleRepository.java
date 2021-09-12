9f932309188b357df4e20dbab3f58df5c201bfd1
rodriguesrafael-dev <rodriguesrafael@RODRAF83-SQ52298.localdomain>
2021-09-12 19:17:38 -0300

    commit 09.12.2021

---
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dsvendas.repositories;
+
+import com.devsuperior.dsvendas.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author rodriguesrafael
+ */
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
---
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dsvendas.repositories;
+
+import com.devsuperior.dsvendas.entities.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author rodriguesrafael
+ */
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
