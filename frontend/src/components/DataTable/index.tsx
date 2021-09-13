a0f050acff22db06b0b564053a1a902e44edee2e
rodriguesrafael-dev <rodriguesrafael@RODRAF83-SQ52298.localdomain>
2021-09-13 00:32:08 -0300

    commit 09.12.2021

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/requests";
 
 function DataTable() {
